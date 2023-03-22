6440791be418a874247f0c7f92793a251a5814b2
Justin Campbell <54825235+Justin-Campbell@users.noreply.github.com>
2023-03-21 21:07:03 -0600

    New pipeline for plotting individual pt brains
    - get3DCoords.m can be used to extract variables for plotting brains and electrodes in both patient and space
    - plotPtBrain.py is used for plotting individual pt brains

---
+++ get3DCoords.m
@@ -0,0 +1,40 @@
+function get3DCoords(pID, savepath)
+% get3DCoords takes a patient ID (pID) as input and the path for where 
+% to save the data (savepath) to export the following:
+%
+%   XYZ_MNI:    coordinates of electrode contacts in MNI space
+%   XYZ:        coordinates of electrode contacts in patient space
+%   vertices:   vertices for 3D model of patient brain
+%   faces:      faces for 3D model of patient brain
+%
+% e.g., get3DCoords('UIC202302', 'G:\My Drive\Data\UIC202302\')
+% Justin Campbell (03/21/23)
+%%
+
+% Load preprocessed files (LeGUI pipeline)
+ptPath = strcat('Z:\', pID, '\Imaging\Registered\');
+load(strcat(ptPath, 'Electrodes.mat')); % load electrode info
+load(strcat(ptPath, 'Surfaces.mat')); % load 3d model info
+
+% Extract coordinates
+XYZ_MNI     = ElecXYZMNIRaw;
+XYZ         = ElecXYZRaw;
+vertices    = BrainSurfRaw.vertices;
+faces       = BrainSurfRaw.faces;
+
+% Remove microelectrodes
+XYZ(MicroElecRaw,:) = NaN;
+XYZ = rmmissing(XYZ);
+XYZ_MNI(MicroElecRaw,:) = NaN;
+XYZ_MNI = rmmissing(XYZ_MNI);
+
+% Export
+writematrix(XYZ_MNI, fullfile(savepath, strcat(pID, '_XYZ_MNI.csv')));
+writematrix(XYZ, fullfile(savepath, strcat(pID, '_XYZ.csv')));
+writematrix(vertices, fullfile(savepath, strcat(pID, '_vertices.csv')));
+writematrix(faces, fullfile(savepath, strcat(pID, '_faces.csv')));
+
+% Display
+fprintf('Files saved to %s \n', savepath);
+
+end
--- plotPtBrain.py
+++ plotPtBrain.py
@@ -1,9 +1,11 @@
-def plotBLAESStim(xyz, dPrime = None, faces = None, vertices = None, roi_smoothing = 8, elec_size = 20, show_brain = True):
-    """plotBLAESStim() uses the visbrain package (visbrain.org) to generate a 3D model of an MNI brain w/ electrode contacts superimposed. Electrode locations (MNI XYZ coordinates) are defined in each patient's CSMap.mat file.
+def plotPtBrain(xyz, dPrime = None, faces = None, vertices = None, roi_smoothing = 8, elec_size = 16, show_brain = True):
+    """plotPtBrain() uses the visbrain package (visbrain.org) to generate a 3D model of a specific patient's brain w/ electrode contacts superimposed. Electrode locations (MNI XYZ coordinates) are defined in each patient's CSMap.mat file.
 
     Inputs:
         xyz (pd.DataFrame): X,Y,Z coordinates of electrodes (use MNI space if >1 patient).
         dPrime (pd.DataFrame, optional): dPrime values from behavioral testing; used to color electrodes.
+        faces (pd.DataFrame, optional): faces for 3D model of patient brain.
+        vertices (pd.DataFrame, optional): vertices for 3D model of patient brain.
         elec_size (float, optional): Marker size for electrode contact(s).
         roi_smoothing (int, optional): Amount of smoothing to apply to ROI voxels.
         
@@ -19,7 +21,7 @@ def plotBLAESStim(xyz, dPrime = None, faces = None, vertices = None, roi_smoothi
     
     # Brain object(s)
     if show_brain == True:
-        brain_obj = BrainObj('B3', translucent = True)
+        brain_obj = BrainObj('white', faces = faces, vertices = vertices, translucent = True)
         scene_obj.add_to_subplot(brain_obj, row=0, col=0, use_this_cam=True)
     
     # ROI object(s)
@@ -47,11 +49,11 @@ def plotBLAESStim(xyz, dPrime = None, faces = None, vertices = None, roi_smoothi
 
 ### IMPLEMENTATION
 import pandas as pd
-datapath = '/Users/justincampbell/Library/CloudStorage/GoogleDrive-u0815766@gcloud.utah.edu/My Drive/Research Projects/BLAES/Data/BLAESCoords 021523.csv'
-BLAESCoords = pd.read_csv(datapath)
-xyz = BLAESCoords[['X', 'Y', 'Z']].to_numpy()
-dPrime = BLAESCoords['dPrimeDiff_Item'].to_numpy()
+datapath = '/Users/justincampbell/Library/CloudStorage/GoogleDrive-u0815766@gcloud.utah.edu/My Drive/Research Projects/BLAES/Data/UIC202302/UIC202302_XYZ.csv'
+xyz = pd.read_csv(datapath).to_numpy()
+faces = pd.read_csv('/Users/justincampbell/Library/CloudStorage/GoogleDrive-u0815766@gcloud.utah.edu/My Drive/Research Projects/BLAES/Data/UIC202302/UIC202302_faces.csv')
+vertices = pd.read_csv('/Users/justincampbell/Library/CloudStorage/GoogleDrive-u0815766@gcloud.utah.edu/My Drive/Research Projects/BLAES/Data/UIC202302/UIC202302_vertices.csv')    
 
 if __name__ == "__main__":
-    plotBLAESStim(xyz, dPrime) # MNI
+    plotPtBrain(xyz, faces = faces, vertices = vertices) # patient brain
     
